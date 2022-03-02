17ec26654490971b455e3baddac63b421526da50
Ivan Bozveliev <ivanbozveliev@abv.bg>
2022-03-02 22:52:44 +0200

    change nav

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -5,16 +5,22 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <div className='navDiv'>
+
       <div className='Logo'>
         <img src='../../img/logo1.png' />
         <p>Pizza Borano</p>
       </div>
 
-      <Link to="#">About</Link>
-      <Link to="#">Order</Link>
-      <Link to="/login">Login</Link>
-      <Link to="contacts">Contacts</Link>
-      <Link to="/">Home</Link>
+      <div className='allLinks'>
+        
+        <Link to="/">Home</Link>
+        <Link to="contacts">Contacts</Link>
+        <Link to="/login">Login</Link>
+        <Link to="#">About</Link>
+        <Link to="#">Order</Link>
+     
+      </div>
+
 
     </div>
   )
