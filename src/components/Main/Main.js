411971a0c6371618c8d57b1648f04a0b134a2426
Ivan Bozveliev <ivanbozveliev@abv.bg>
2022-03-04 20:16:24 +0200

    add carousel logic

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,46 +1,53 @@
 import './Main.css';
 import Card from './Card/Card.js';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { carouselFunction } from './CarouselLogic.js';
+
+let allCards = carouselFunction();
 
 const Main = () => {
 
-  const cardsInfo = [
-    { id: 1, img: '../../../img/pizza5.png', title: 'pizza' },
-    { id: 2, img: '../../../img/pizza8.png', title: 'pizza1' },
-    { id: 3, img: '../../../img/pizza6.png', title: 'pizza2' },
-    { id: 4, img: '../../../img/pizza10.png', title: 'pizza3' },
-    { id: 5, img: '../../../img/pizza11.png', title: 'pizza4' },
-    { id: 6, img: '../../../img/pizza12.png', title: 'pizza5' }
-  ]
-
-  let cards = cardsInfo.slice(0, 3);
+  let cards = allCards.slice(0, 3);
 
   let [state, setState] = useState(cards);
+  let [counter, setCounter] = useState(0);
+  let [prevCounter, setPrevCounter] = useState(0);
   let [style, setStyle] = useState('mainStyle');
 
-  const leftClick = (e) => {
-    setStyle('leftStyle')
-    let cards = cardsInfo.slice(0, 3);
-    setState(cards)
 
-  }
+  useEffect(() => {
 
-  const rightClick = () => {
-    setStyle('rightStyle')
-    let cards = cardsInfo.slice(3, 6);
-    setState(cards)
+    if (0 > counter || counter >= allCards.length) {
+      return;
+    } else {
+      if (prevCounter < counter) {
 
-  }
+        setStyle('rightStyle');
+        setPrevCounter(counter);
+
+      } else if (prevCounter > counter) {
+        setStyle('leftStyle')
+        setPrevCounter(counter);
+      }
+
+      setState(allCards[counter]);
+    }
+
+
+
+  }, [counter])
+
+
 
   return (
     <div className='container'>
       <div className='carousel'>
-        <img id='imgLeft' src='../../../img/leftArrow.png' onClick={leftClick} />
+        <img id='imgLeft' src='../../../img/leftArrow.png' onClick={() => setCounter(--counter)} />
 
         <div className='gridDiv'>
           {state.map(x => <Card key={x.id} img={x.img} title={x.title} currentStyle={style} />)}
         </div>
-        <img src='../../../img/rightArrow.png' id='imgRight' onClick={rightClick} />
+        <img src='../../../img/rightArrow.png' id='imgRight' onClick={() => setCounter(++counter)} />
       </div>
 
     </div>
