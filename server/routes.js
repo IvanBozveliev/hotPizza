5df2e5a92defc380576995aa52f436808d393c7d
Ivan Bozveliev <ivanbozveliev@abv.bg>
2022-03-11 21:37:20 +0200

    create routes,userController and userService for the user on the server side

--- server/routes.js
+++ server/routes.js
@@ -5,10 +5,12 @@ const router = Router();
 const feedbackController = require('./controllers/feedbackController');
 const productController = require('./controllers/productController');
 const authController = require('./controllers/authController');
+const usersController = require('./controllers/userController');
 
 router.use('/api/products', productController);
 router.use('/api/auth', authController);
-router.use('/api/feedback', feedbackController)
+router.use('/api/feedback', feedbackController);
+router.use('/api/users', usersController)
 // router.get("*" ,(req, res) =>{
 //     res.sendFile(path.join(__dirname,'public', 'index.html'))
 // });
