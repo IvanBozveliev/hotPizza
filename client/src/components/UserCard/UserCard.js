f9b4bd8fc0dc411b5cbfdf5f4d8560e2e809a7e8
Ivan Bozveliev <ivanbozveliev@abv.bg>
2022-03-12 12:29:25 +0200

    creating style for users page

--- client/src/components/UserCard/UserCard.js
+++ client/src/components/UserCard/UserCard.js
@@ -11,9 +11,12 @@ const UserCard = ({
             <div className='usersInfo'>
                 <h4 id='userName'>{username}</h4>
                 <p id='currentRole'>{roles}</p>
-                <button>client</button>
-                <button>editor</button>
-                <button>admin</button>
+                <div id='allBtns'>
+                    <button id={roles == 'client' ? roles : 'clientBtn'}>client</button>
+                    <button id={roles == 'editor' ? roles : 'editorBtn'}>editor</button>
+                    <button id={roles == 'admin' ? roles : 'adminBtn'}>admin</button>
+                </div>
+
                 <p id='delUserBtn'><p id='txtDel'>X</p></p>
             </div>
 
