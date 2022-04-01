c2bac8f73cea682c8e4baa4012e63007ec30d527
Ivan Bozveliev <ivanbozveliev@abv.bg>
2022-04-01 11:57:36 +0300

    create logic for buy button and create error page component

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,16 @@ import CreatePizza from './components/CreatePizza/CreatePizza.js';
 import About from './components/About/About.js';
 import Cart from './components/Cart/Cart';
 import Details from './components/DetailsProduct/DetailsProduct';
-// import AllProducts from './components/AllProducts/AllProducts.js';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 import EditProduct from './components/EditProduct/EditProduct.js';
 import Users from './components/Users/Users';
 import FeedBack from './components/FeedBack/FeedBack.js';
 import Logout from './components/Logout/Logout';
-
+import EditFeedback from './components/EditFeedback/EditFeedback.js';
 import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
-import EditFeedback from './components/EditFeedback/EditFeedback.js';
+
 
 function App() {
   return (
@@ -32,12 +32,12 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/details/:id' element={<Details />} />
-          {/* <Route path='/all-products' element={<AllProducts />} /> */}
           <Route path='/details/edit/:id' element={<EditProduct />} />
           <Route path='/feedback' element={<FeedBack />} />
           <Route path='/feedback/edit/:id' element={<EditFeedback />} />
           <Route path='/users' element={<Users />} />
           <Route path='/logout' element={<Logout />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </div>
 
--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -3,10 +3,11 @@ import Product from './Product/Product';
 // import {isAuth} from '../../hoc/isAuth'
 import { connect } from 'react-redux';
 import { getAllCartProducts, increaseQty, decreaseQty, deleteCart, deleteAllCartProducts } from '../../actions/cartActions';
-
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
-
+    const [modal, setModal] = useState(false);
     let totalCart = 0;
 
     props.cart.data.map(prod => {
@@ -26,8 +27,28 @@ const Cart = (props) => {
         props.deleteCart(product)
     }
 
+
+    const onHandler = () => {
+        setModal(false)
+        props.deleteAllCartProducts()
+    }
     return (
         <>
+            {totalCart !== 0 && modal ? (
+
+                <div className='BuyModal'>
+                    <div className='BuyModalContent'>
+                        <p id='buyTextModal'>Your payment was successful!</p>
+                        <img id="payment" src='../../../../img/payment.png' width={45} height={45} />
+                        <p><b>You`ve paid:</b> {`${totalCart}`} lv</p>
+                        <button id='buymodalCancel' onClick={() => onHandler()}>Return</button>
+                        <p id='txtbuy' >You can see your orders <Link to='/orders' onClick={() => onHandler()}>here</Link> </p>
+                    </div>
+
+                </div>)
+                : ''
+            }
+
 
             <div className="cartBackground">
 
@@ -46,7 +67,7 @@ const Cart = (props) => {
                 <div id='totalDiv'>
                     <p><b>Total:</b></p>
                     <p id='price'>{totalCart}.00 lv</p>
-                    <button id='buy'>BUY</button>
+                    <button id='buy' onClick={() => setModal(true)}>BUY</button>
                     <button id='cancel' onClick={() => props.deleteAllCartProducts()}>CLEAR</button>
                 </div>
 
